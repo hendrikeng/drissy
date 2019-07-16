70d3336498f53fde0593bfc5fad7bbf510f8ac9f
Hendrikeng <hendrik@wewereyoung.de>
2019-07-17 00:11:20 +0200

    raus

--- webpack.settings.js
+++ webpack.settings.js
@@ -154,8 +154,8 @@ module.exports = {
     },
     workboxConfig: {
         swDest: '../sw.js',
-        clientsClaim: true, //new
-        skipWaiting: true, //new
+        // clientsClaim: true, //new
+        // skipWaiting: true, //new
         precacheManifestFilename: 'js/precache-manifest.[manifestHash].js',
         // importScripts: ['/dist/workbox-catch-handler.js'],
         exclude: [
