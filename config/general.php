0c42177e973a9086deeb9b39708b050319ed4796
Hendrik Werner <hendrik@wewereyoung.de>
2019-08-04 15:49:04 +0200

    updated general config

--- config/general.php
+++ config/general.php
@@ -13,7 +13,17 @@ return [
     '*' => [
         // Default Week Start Day (0 = Sunday, 1 = Monday...)
         'defaultWeekStartDay' => 1,
-
+        //
+        'cacheDuration' => false,
+        //
+        'defaultSearchTermOptions' => array(
+            'subLeft' => true,
+            'subRight' => true,
+        ),
+        //
+        'enableCsrfProtection' => true,
+        //
+        'generateTransformsBeforePageLoad' => true,
         // Whether generated URLs should omit "index.php"
         'omitScriptNameInUrls' => true,
 
@@ -22,7 +32,10 @@ return [
 
         // The secure key Craft will use for hashing and encrypting data
         'securityKey' => getenv('SECURITY_KEY'),
-
+        //
+        'useEmailAsUsername' => true,
+        //
+        'usePathInfo' => true,
         // Whether to save the project config out to config/project.yaml
         // (see https://docs.craftcms.com/v3/project-config.html)
         'useProjectConfigFile' => false,
@@ -32,17 +45,45 @@ return [
     'dev' => [
         // Dev Mode (see https://craftcms.com/guides/what-dev-mode-does)
         'devMode' => true,
+        //
+        'allowUpdates' => true,
+        //
+        'backupOnUpdate' => true,
+        //
+        'enableTemplateCaching' => false,
+        //
+        'isSystemOn' => true,
     ],
 
     // Staging environment settings
     'staging' => [
         // Set this to `false` to prevent administrative changes from being made on staging
         'allowAdminChanges' => true,
+        //
+        'allowUpdates' => false,
+        //
+        'backupOnUpdate' => false,
+        //
+        'devMode' => false,
+        //
+        'enableTemplateCaching' => true,
+        //
+        'isSystemOn' => false,
     ],
 
     // Production environment settings
     'production' => [
         // Set this to `false` to prevent administrative changes from being made on production
         'allowAdminChanges' => true,
+        //
+        'allowUpdates' => false,
+        //
+        'backupOnUpdate' => false,
+        //
+        'devMode' => false,
+        //
+        'enableTemplateCaching' => true,
+        //
+        'isSystemOn' => true,
     ],
 ];
