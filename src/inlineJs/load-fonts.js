0e6fe8c16a14e52132d7463bdbc9564e59ddb417
Hendrik Werner <hendrik@wewereyoung.de>
2019-09-18 13:50:31 +0200

    remove session storage

--- src/inlineJs/load-fonts.js
+++ src/inlineJs/load-fonts.js
@@ -4,6 +4,5 @@ var FontA = new FontFaceObserver("Roboto Condensed");
 var FontB = new FontFaceObserver("Cabin");
 
 Promise.all([FontA.load(), FontB.load()]).then(function () {
-    document.documentElement.className += "fonts-loaded";
-    sessionStorage.foftFontsLoaded = true;
+    document.documentElement.className += " fonts-loaded FO ";
 });
