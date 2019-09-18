2172fd948bddd1682ce9ba9b88b4bee57a0c3c39
Hendrik Werner <hendrik@wewereyoung.de>
2019-09-18 14:05:38 +0200

    fix fontface

--- src/inlineJs/load-fonts.js
+++ src/inlineJs/load-fonts.js
@@ -4,5 +4,5 @@ var FontA = new FontFaceObserver("Roboto Condensed");
 var FontB = new FontFaceObserver("Cabin");
 
 Promise.all([FontA.load(), FontB.load()]).then(function () {
-    document.documentElement.className += " fonts-loaded FO ";
+    document.documentElement.className += "fonts-loaded";
 });
