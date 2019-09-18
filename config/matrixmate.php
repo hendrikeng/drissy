b374a3e24595096ac21d8f8b08f88008d9383bab
Hendrik Werner <hendrik@wewereyoung.de>
2019-09-19 01:28:07 +0200

    added matrix mate

---
+++ config/matrixmate.php
@@ -0,0 +1,144 @@
+<?php
+
+    return [
+        'fields' => [
+            'contentBuilder' => [
+                'hideUngroupedTypes' => true,
+                'groups' => [
+                    [
+                        'label' => 'Elements',
+                        'types' => [
+                            'headline',
+                            'richText',
+                            'quote',
+                            'form',
+                            'embedded',
+                            'separator',
+                        ],
+                    ],
+                    [
+                        'label' => 'Image',
+                        'types' => [
+                            'imageSingle',
+                            'imageGrid',
+                            'imageSlider',
+                        ],
+                    ],
+                    [
+                        'label' => 'Category',
+                        'types' => [
+                            'category',
+                            'categorySlider',
+                        ],
+                    ],
+                    [
+                        'label' => 'Entry',
+                        'types' => [
+                            'entry',
+                            'entrySlider',
+                        ],
+                    ]
+                ],
+                'types'  => [
+                    'imageSingle' => [
+                        'tabs' => [
+                            [
+                                'label'  => 'Content',
+                                'fields' => [
+                                    'image',
+                                ],
+                            ],
+                            [
+                                'label'  => 'Settings',
+                                'fields' => [ 'ratio', 'caption' ],
+                            ],
+                        ],
+                    ],
+                    'imageGrid' => [
+                        'tabs' => [
+                            [
+                                'label'  => 'Content',
+                                'fields' => [
+                                    'images',
+                                ],
+                            ],
+                            [
+                                'label'  => 'Settings',
+                                'fields' => [ 'ratio', 'columns', 'caption' ],
+                            ],
+                        ],
+                    ],
+                    'imageSlider' => [
+                        'tabs' => [
+                            [
+                                'label'  => 'Content',
+                                'fields' => [
+                                    'images',
+                                ],
+                            ],
+                            [
+                                'label'  => 'Settings',
+                                'fields' => [ 'layout' ],
+                            ],
+                        ],
+                    ],
+                    'category' => [
+                        'tabs' => [
+                            [
+                                'label'  => 'Content',
+                                'fields' => [
+                                    'category',
+                                ],
+                            ],
+                            [
+                                'label'  => 'Settings',
+                                'fields' => [ 'layout', 'linkToEntry' ],
+                            ],
+                        ],
+                    ],
+                    'categorySlider' => [
+                        'tabs' => [
+                            [
+                                'label'  => 'Content',
+                                'fields' => [
+                                    'category',
+                                ],
+                            ],
+                            [
+                                'label'  => 'Settings',
+                                'fields' => [ 'layout', 'linkToEntry', 'limit' ],
+                            ],
+                        ],
+                    ],
+                    'entry' => [
+                        'tabs' => [
+                            [
+                                'label'  => 'Content',
+                                'fields' => [
+                                    'entry',
+                                ],
+                            ],
+                            [
+                                'label'  => 'Settings',
+                                'fields' => [ 'layout', 'linkToEntry' ],
+                            ],
+                        ],
+                    ],
+                    'entrySlider' => [
+                        'tabs' => [
+                            [
+                                'label'  => 'Content',
+                                'fields' => [
+                                    'entry',
+                                ],
+                            ],
+                            [
+                                'label'  => 'Settings',
+                                'fields' => [ 'layout', 'linkToEntry' ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ];
